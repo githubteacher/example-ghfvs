c2d4a150bca49d865f1ac7020fd479763750b63a
GitHub Teacher <trainingdemos+githubteacher@github.com>
2015-05-05 12:33:02 -0500

    Add solution files

---
+++ GitHub-for-Visual-Studio/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitHub_for_Visual_Studio
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
